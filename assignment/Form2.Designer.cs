b5599974c0fb68eca0156dfc29cca466dfecbab9
Gareth William David Appleby <G.Appleby6662@student.leedsbeckett.ac.uk>
2018-01-05 12:18:17 +0000

    lost previous commits, adding back rest of repos.

--- assignment/Form2.Designer.cs
+++ assignment/Form2.Designer.cs
@@ -38,6 +38,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.Cause = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbxstudents
@@ -122,11 +123,21 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Cause";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 509);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.Cause);
             this.Controls.Add(this.button1);
@@ -156,6 +167,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox Cause;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
